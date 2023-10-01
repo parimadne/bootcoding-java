f73a46caed99304e447479d47642b32e301c2fb5
parimadne <parimadne04@gmail.com>
2023-10-02 00:26:36 +0530

    Java Program

--- in/array/Array9.java
+++ in/array/Array9.java
@@ -1,5 +1,4 @@
 package in.array;
-
 public class Array9 {
     public static void main(String[] args) {
         int[] a = {10, 15, 20, 25, 35, 75, 80, 95};
@@ -7,7 +6,6 @@ public class Array9 {
         for (int ele : a) {
             System.out.print(ele + " ");
         }
-
         int[] b = swap(a);
             System.out.println("");
         for(int ele: b) {
@@ -18,8 +16,7 @@ public class Array9 {
         for(int ele : c){
             System.out.print(ele+" ");
     }
-    //swapping first no. with last no.\
-    // swapping first three no. with last three no.
+    //swapping first no. with last no.
     }
     public static int[] swap(int[] b) {
         int temp = b[0];
@@ -27,6 +24,7 @@ public class Array9 {
         b[b.length - 1] = temp;
         return b;
     }
+    // swapping first three no. with last three no.
     public static int[] swap3(int[] c){
         for(int i =0 ; i<3;i++){
             int temp = c[i];
