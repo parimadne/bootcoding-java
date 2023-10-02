e37d13d0846941fb0510129e5b5f107e75714ce9
parimadne <parimadne04@gmail.com>
2023-10-03 00:10:28 +0530

    Java Program

---
+++ in/array/Array0.java
@@ -0,0 +1,10 @@
+package in.array;
+
+public class Array0{
+    public static void main(String[] args) {
+        int[] a={10,15,20,25,30};
+        for (int j : a) {
+            System.out.print(j + " ");
+        }
+    }
+}
