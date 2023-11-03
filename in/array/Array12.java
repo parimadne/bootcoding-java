d8f6839b6c8f4378cb2f1dcac697e9c29ca5ab2e
parimadne <parimadne04@gmail.com>
2023-11-04 00:04:20 +0530

    Java Program

---
+++ in/array/Array12.java
@@ -0,0 +1,12 @@
+package in.array;
+import java.util.Scanner;
+public class Array12 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n= sc.nextInt();
+        String[] a= new String[n];
+        for(int i=0;i<a.length;i++){
+            System.out.println(a[i]);
+        }
+    }
+}
